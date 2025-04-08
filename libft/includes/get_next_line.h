d8a3fb1fec914ce52fa181b4a610db43774f0451
Hugo Ganet <ganethugo@gmail.com>
2025-04-08 10:45:31 +0200

    Embed libft source into pipex project (not a submodule)

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -1,20 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hganet <hganet@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 10:07:15 by hganet            #+#    #+#             */
+/*   Updated: 2025/04/08 10:07:58 by hganet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
+# include "libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 128
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
 
 char	*get_next_line(int fd);
-char 	*ft_strjoin_gnl(char *s1, char *s2, size_t size);
-// void	*ft_calloc(size_t nmemb, size_t size);
-// char	*ft_strchr(const char *s, int c);
-// size_t	ft_strlen(const char *s);
+char	*ft_strjoin_gnl(char *s1, char *s2, size_t size);
 
 #endif
